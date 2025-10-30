b2f87a8e839a5576600430a67321814c8009da7c
UltralyticsAssistant <web@ultralytics.com>
2025-10-30 15:14:13 +0000

    Auto-format by https://ultralytics.com/actions

--- train.py
+++ train.py
@@ -1,15 +1,18 @@
-from ultralytics import RTDETR
 import warnings
-warnings.filterwarnings('ignore')
+
+from ultralytics import RTDETR
+
+warnings.filterwarnings("ignore")
 
 model = RTDETR(r"ultralytics/cfg/models/rt-detr/rtdetr-resnet50.yaml")
 # model.load('rtdetr-l.pt') # 是否加载预训练权重
-model.train(data='data.yaml',  # 训练参数均可以重新设置
-                        epochs=100,
-                        imgsz=640,
-                        workers=4,
-                        batch=16,
-                        device='cpu',
-                        optimizer='AdamW',
-                        amp=False,
-                        )
+model.train(
+    data="data.yaml",  # 训练参数均可以重新设置
+    epochs=100,
+    imgsz=640,
+    workers=4,
+    batch=16,
+    device="cpu",
+    optimizer="AdamW",
+    amp=False,
+)
--- val.py
+++ val.py
@@ -1,5 +1,5 @@
 from ultralytics import RTDETR
 
-if __name__ == '__main__':
-    model = RTDETR(model='runs/train/exp/weights/best.pt')
-    model.val(data='data.yaml', batch=32, device='0', imgsz=640, workers=8, split='test')
+if __name__ == "__main__":
+    model = RTDETR(model="runs/train/exp/weights/best.pt")
+    model.val(data="data.yaml", batch=32, device="0", imgsz=640, workers=8, split="test")
