3a3647da3d4a5935ca1336106e903e8525322868
LaughingFox <zhengsheng0910@gmail.com>
2025-11-03 02:53:05 +0800

    resume train.py

--- detect.py
+++ detect.py
@@ -4,8 +4,8 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR(r"C:\Users\60174\Downloads\best.pt")
-    model.predict(source=r"C:\Users\60174\OneDrive\FYP\dataset\selected_category_dataset\images\test\7_Types_Plastic_test_1893_IMG_2826_jpg.rf.cd3d0f143c65b141c21658b2ab0142cf.jpg",
+    model = RTDETR(r"C:\Users\60174\Downloads\rtdetr-resnet50-weights\best.pt")
+    model.predict(source=r"C:\Users\60174\Downloads\download (10).jpg",
                   imgsz=640,
                   device='cpu',
                   save=True
--- train.py
+++ train.py
@@ -2,14 +2,16 @@ from ultralytics import RTDETR
 import warnings
 warnings.filterwarnings('ignore')
 
-model = RTDETR(r"ultralytics/cfg/models/rt-detr/rtdetr-x.yaml")
-# model.load('rtdetr-l.pt') # 是否加载预训练权重
-model.train(data='data.yaml',  # 训练参数均可以重新设置
-                        epochs=100,
-                        imgsz=640,
-                        workers=4,
-                        batch=16,
-                        device=0,
-                        optimizer='AdamW',
-                        amp=False,
-                        )
+# model = RTDETR(r"ultralytics/cfg/models/rt-detr/rtdetr-x.yaml")
+# model.train(data='data.yaml',
+#                         epochs=100,
+#                         imgsz=640,
+#                         workers=4,
+#                         batch=16,
+#                         device=0,
+#                         optimizer='AdamW',
+#                         amp=False,
+#                         )
+
+model = RTDETR('runs/detect/train/weights/last.pt')
+results = model.train(resume=True)
