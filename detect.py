f5b3f5ee15ddb10a23b6640f5a4db67b4ef92a34
LaughingFox <zhengsheng0910@gmail.com>
2025-11-02 12:04:48 +0800

    update class to 22

--- detect.py
+++ detect.py
@@ -4,10 +4,10 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR('runs/train/exp/weights/best.pt')
-    model.predict(source='images',
+    model = RTDETR(r"C:\Users\60174\Downloads\best.pt")
+    model.predict(source=r"C:\Users\60174\OneDrive\FYP\dataset\selected_category_dataset\images\test\7_Types_Plastic_test_1893_IMG_2826_jpg.rf.cd3d0f143c65b141c21658b2ab0142cf.jpg",
                   imgsz=640,
-                  device='0',
+                  device='cpu',
                   save=True
                   )
 
--- val.py
+++ val.py
@@ -1,5 +1,5 @@
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR(model='runs/train/exp/weights/best.pt')
+    model = RTDETR(model=r"C:\Users\60174\Downloads\best.pt")
     model.val(data='data.yaml', batch=32, device='0', imgsz=640, workers=8, split='test')
