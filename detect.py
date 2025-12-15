60c4dd6b5ace7f42d45244ada925e7df4f9a6296
UltralyticsAssistant <web@ultralytics.com>
2025-12-15 04:27:05 +0000

    Auto-format by https://ultralytics.com/actions

--- detect.py
+++ detect.py
@@ -1,13 +1,8 @@
 import warnings
 
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 from ultralytics import RTDETR
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     model = RTDETR(r"C:\Users\60174\Downloads\best2.pt")
-    model.predict(source=r"C:\Users\60174\Downloads\images (4).jpg",
-                  imgsz=640,
-                  device='cpu',
-                  save=True
-                  )
-
+    model.predict(source=r"C:\Users\60174\Downloads\images (4).jpg", imgsz=640, device="cpu", save=True)
